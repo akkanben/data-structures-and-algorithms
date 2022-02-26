86628c5970d7ce3e65d986582e8adad2e54174b6
Ben Mills <fuzzyllama@gmail.com>
2022-02-25 16:50:38 -0800

    Finishes queue and node classes

--- java/datastructures/lib/src/main/java/datastructures/queue/Node.java
+++ java/datastructures/lib/src/main/java/datastructures/queue/Node.java
@@ -1,4 +1,14 @@
 package datastructures.queue;
 
-public class Node {
+public class Node<T> {
+  public T value;
+  public Node<T> next = null;
+
+  public Node(T value) {
+    this.value = value;
+  }
+
+  public Node() {
+    // empty
+  }
 }
