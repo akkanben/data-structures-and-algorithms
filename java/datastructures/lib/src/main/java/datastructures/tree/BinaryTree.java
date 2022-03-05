af858670ee92bbaa0a4ab7d412d49fa9ab3a7691
Ben Mills <fuzzyllama@gmail.com>
2022-03-04 16:39:07 -0800

    Adds working binary tree printing values

--- java/datastructures/lib/src/main/java/datastructures/tree/BinaryTree.java
+++ java/datastructures/lib/src/main/java/datastructures/tree/BinaryTree.java
@@ -1,4 +1,49 @@
 package datastructures.tree;
 
-public class BinaryTree {
+import datastructures.queue.Queue;
+
+public class BinaryTree<T> {
+
+
+  public BinaryTree() {
+    //empty;
+  }
+
+  public void preOrderTraversal(Node<T> root) {
+    System.out.print(root.value + " ");
+    if (root.left != null)
+      preOrderTraversal(root.left);
+    if (root.right != null)
+      preOrderTraversal(root.right);
+  }
+
+  public void inOrderTraversal(Node<T> root) {
+    if (root.left != null)
+      preOrderTraversal(root.left);
+    System.out.print(root.value + " ");
+    if (root.right != null)
+      preOrderTraversal(root.right);
+  }
+
+  public void postOrderTraversal(Node<T> root) {
+    if (root.left != null)
+      preOrderTraversal(root.left);
+    if (root.right != null)
+      preOrderTraversal(root.right);
+    System.out.print(root.value + " ");
+  }
+
+  public void breadthFirstTraversal(Node<T> root) {
+    Queue<Node<T>> breadthQueue = new Queue<>();
+    breadthQueue.enqueue(root);
+    Node<T> front;
+    while(!breadthQueue.isEmpty()) {
+      front = breadthQueue.dequeue();
+      System.out.print(front.value + " ");
+      if (front.left != null)
+        breadthQueue.enqueue(front.left);
+      if (front.right != null)
+        breadthQueue.enqueue(front.right);
+    }
+  }
 }
--- java/datastructures/lib/src/main/java/datastructures/tree/Node.java
+++ java/datastructures/lib/src/main/java/datastructures/tree/Node.java
@@ -1,4 +1,17 @@
 package datastructures.tree;
 
-public class Node {
+public class Node<T> {
+
+  T value;
+  Node<T> left = null;
+  Node<T> right = null;
+
+  public Node() {
+    //empty
+  }
+
+  public Node(T value) {
+    this.value = value;
+  }
+
 }
