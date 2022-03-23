94bf0a5d0666a04f4e203b83cbadc5c0e2ec9171
Ben Mills <fuzzyllama@gmail.com>
2022-03-23 16:04:01 -0700

    Adds quicksort and helper functions

--- java/datastructures/lib/src/main/java/codechallenges/SortChallenges.java
+++ java/datastructures/lib/src/main/java/codechallenges/SortChallenges.java
@@ -1,8 +1,5 @@
 package codechallenges;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class SortChallenges {
 
   public void insertionSort(int[] arr) {
@@ -53,5 +50,33 @@ public class SortChallenges {
       arr[k++] = right[j++];
   }
 
+  public void quickSort(int[] arr) {
+    quickHelper(arr, 0,arr.length - 1);
+  }
+
+  private void quickHelper(int[] arr, int left, int right) {
+    if (left < right) {
+      int position = partition(arr, left, right);
+      quickHelper(arr, left, position - 1);
+      quickHelper(arr, position +1, right);
+    }
+  }
+
+  private int partition(int[] arr, int left, int right) {
+    int pivot = arr[right];
+    int low = left - 1;
+    for (int i = left; i < right; i++) {
+      if (arr[i] <= pivot)
+        swap(arr, i, ++low);
+    }
+    swap(arr, right, low + 1);
+    return low + 1;
+  }
+
+  private void swap(int[] arr, int i, int low) {
+    int temp = arr[i];
+    arr[i] = arr[low];
+    arr[low] = temp;
+  }
 
 }
