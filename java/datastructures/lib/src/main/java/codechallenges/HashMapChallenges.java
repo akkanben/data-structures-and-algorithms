2ba00f12008e1eaeb2e4d4f16c4f0e601dd0faa0
Ben Mills <fuzzyllama@gmail.com>
2022-03-28 15:26:32 -0700

    Adds stretch goal for counting words in separate function

--- java/datastructures/lib/src/main/java/codechallenges/HashMapChallenges.java
+++ java/datastructures/lib/src/main/java/codechallenges/HashMapChallenges.java
@@ -27,4 +27,25 @@ public class HashMapChallenges {
     return "";
   }
 
+  public List<String> getWordCounts(String str) {
+    HashMap<String, Integer> hashmap = new HashMap<>();
+    String current = "";
+    for (int i = 0; i < str.length(); i++) {
+      char c = Character.toLowerCase(str.charAt(i));
+      if ((c >= 'a' && c <= 'z') || c == '\'')
+        current = current + c;
+      else if ((c == ' ' || i == str.length() - 1) && current.length() > 0) {
+        if (hashmap.contains(current))
+          hashmap.set(current, hashmap.get(current) + 1);
+        else
+          hashmap.set(current, 1);
+        current = "";
+      }
+    }
+    ArrayList<String> output = new ArrayList<>();
+    for (String element : hashmap.keys()) {
+      output.add(element + ": " + hashmap.get(element));
+    }
+    return output;
+  }
 }
