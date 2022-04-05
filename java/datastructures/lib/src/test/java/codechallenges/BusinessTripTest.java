ee1feda1fe09ae7673c6cc2cb4eda11e750eee7e
Ben Mills <fuzzyllama@gmail.com>
2022-04-05 15:33:02 -0700

    Adds extra testing

--- java/datastructures/lib/src/test/java/codechallenges/BusinessTripTest.java
+++ java/datastructures/lib/src/test/java/codechallenges/BusinessTripTest.java
@@ -167,4 +167,87 @@ public class BusinessTripTest {
     assertEquals(634, sut.businessTrip(graph, cities));
   }
 
+  @Test
+  void too_few_vertices() {
+    GraphChallenges sut = new GraphChallenges();
+    Graph<String> graph = new Graph<>();
+    Vertex<String> pandora = graph.addVertex("Pandora");
+    String[] cities = {"Pandora", "Arendelle", "Monstropolis", "Naboo", "Narnia", "Metroville", "Pandora"};
+    assertEquals(0, sut.businessTrip(graph, cities));
+  }
+
+  @Test
+  void too_few_cities() {
+    GraphChallenges sut = new GraphChallenges();
+    Graph<String> graph = new Graph<>();
+    Vertex<String> pandora = graph.addVertex("Pandora");
+    Vertex<String> arendelle = graph.addVertex("Arendelle");
+    Vertex<String> metroville = graph.addVertex("Metroville");
+    Vertex<String> monstropolis = graph.addVertex("Monstropolis");
+    Vertex<String> narnia = graph.addVertex("Narnia");
+    Vertex<String> naboo = graph.addVertex("Naboo");
+    graph.addEdge(pandora, arendelle, 150);
+    graph.addEdge(arendelle, pandora, 150);
+    graph.addEdge(pandora, metroville, 82);
+    graph.addEdge(metroville, pandora, 82);
+    graph.addEdge(arendelle, metroville, 99);
+    graph.addEdge(metroville, arendelle, 99);
+    graph.addEdge(arendelle, monstropolis, 42);
+    graph.addEdge(monstropolis, arendelle, 42);
+    graph.addEdge(metroville, monstropolis, 105);
+    graph.addEdge(monstropolis, metroville, 105);
+    graph.addEdge(metroville, naboo, 26);
+    graph.addEdge(naboo, metroville, 26);
+    graph.addEdge(metroville, narnia, 37);
+    graph.addEdge(narnia, metroville, 37);
+    graph.addEdge(narnia, naboo, 250);
+    graph.addEdge(naboo, narnia, 250);
+    graph.addEdge(naboo, monstropolis, 73);
+    graph.addEdge(monstropolis, naboo, 73);
+    String[] cities = {"Pandora"};
+    assertEquals(0, sut.businessTrip(graph, cities));
+  }
+
+
+  @Test
+  void empty_cities() {
+    GraphChallenges sut = new GraphChallenges();
+    Graph<String> graph = new Graph<>();
+    Vertex<String> pandora = graph.addVertex("Pandora");
+    Vertex<String> arendelle = graph.addVertex("Arendelle");
+    Vertex<String> metroville = graph.addVertex("Metroville");
+    Vertex<String> monstropolis = graph.addVertex("Monstropolis");
+    Vertex<String> narnia = graph.addVertex("Narnia");
+    Vertex<String> naboo = graph.addVertex("Naboo");
+    graph.addEdge(pandora, arendelle, 150);
+    graph.addEdge(arendelle, pandora, 150);
+    graph.addEdge(pandora, metroville, 82);
+    graph.addEdge(metroville, pandora, 82);
+    graph.addEdge(arendelle, metroville, 99);
+    graph.addEdge(metroville, arendelle, 99);
+    graph.addEdge(arendelle, monstropolis, 42);
+    graph.addEdge(monstropolis, arendelle, 42);
+    graph.addEdge(metroville, monstropolis, 105);
+    graph.addEdge(monstropolis, metroville, 105);
+    graph.addEdge(metroville, naboo, 26);
+    graph.addEdge(naboo, metroville, 26);
+    graph.addEdge(metroville, narnia, 37);
+    graph.addEdge(narnia, metroville, 37);
+    graph.addEdge(narnia, naboo, 250);
+    graph.addEdge(naboo, narnia, 250);
+    graph.addEdge(naboo, monstropolis, 73);
+    graph.addEdge(monstropolis, naboo, 73);
+    String[] cities = new String[0];
+    assertEquals(0, sut.businessTrip(graph, cities));
+  }
+
+  @Test
+  void empty_graph() {
+    GraphChallenges sut = new GraphChallenges();
+    Graph<String> graph = new Graph<>();
+    String[] cities = {"Nothing", "Potato", "Null", "Nope"};
+    assertEquals(0, sut.businessTrip(graph, cities));
+  }
+
+
 }
